7b25fad426001b5fe14a1d732bd752f37efd5378
sahil <sahil9955555@gmail.com>
2026-06-09 11:40:29 +0530

    Setting basic part of vehicle_maintenance_scheduler

---
+++ vehicle_maintenance_scheduler/src/main/java/com/affordmed/vms/VehicleMaintenanceSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.affordmed.vms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleMaintenanceSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleMaintenanceSchedulerApplication.class, args);
+    }
+
+}
---
+++ vehicle_maintenance_scheduler/src/test/java/com/affordmed/vms/VehicleMaintenanceSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.affordmed.vms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleMaintenanceSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
