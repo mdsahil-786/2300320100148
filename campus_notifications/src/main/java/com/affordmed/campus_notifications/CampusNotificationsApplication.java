63cc04ed9348a742fbc868a85c11c9efb0f0370e
sahil <sahil9955555@gmail.com>
2026-06-09 11:50:27 +0530

    Added campus notifications module

---
+++ campus_notifications/src/main/java/com/affordmed/campus_notifications/CampusNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.affordmed.campus_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusNotificationsApplication.class, args);
+    }
+
+}
---
+++ campus_notifications/src/test/java/com/affordmed/campus_notifications/CampusNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.affordmed.campus_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
