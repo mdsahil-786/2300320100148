efab6232cef6a8cc79c3042f479b8d43eaf5c8a5
sahil <sahil9955555@gmail.com>
2026-06-09 13:44:57 +0530

    All related files of campus_notification are added

---
+++ campus_notifications/src/main/java/com/affordmed/campus_notifications/configs/RestTemplateConfig.java
@@ -0,0 +1,14 @@
+package com.affordmed.campus_notifications.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
