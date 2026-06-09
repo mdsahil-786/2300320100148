0a2876f0198be83969c9de4dc67e0db2775ef49b
sahil <sahil9955555@gmail.com>
2026-06-09 12:39:16 +0530

    Services are added

--- logging_middleware/src/main/java/com/affordmed/logging_middleware/services/AuthService.java
+++ logging_middleware/src/main/java/com/affordmed/logging_middleware/services/AuthService.java
@@ -1,4 +1,31 @@
 package com.affordmed.logging_middleware.services;
 
+import com.affordmed.logging_middleware.client.AuthClient;
+import com.affordmed.logging_middleware.dtos.AuthRequestDTO;
+import com.affordmed.logging_middleware.dtos.AuthResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final AuthClient authClient;
+
+    public String getAccessToken() {
+
+        AuthRequestDTO request = AuthRequestDTO.builder()
+                .email("YOUR_EMAIL")
+                .name("YOUR_NAME")
+                .rollNo("YOUR_ROLLNO")
+                .accessCode("YOUR_ACCESS_CODE")
+                .clientID("YOUR_CLIENT_ID")
+                .clientSecret("YOUR_CLIENT_SECRET")
+                .build();
+
+        AuthResponseDTO response =
+                authClient.getToken(request);
+
+        return response.getAccessToken();
+    }
 }
--- logging_middleware/src/main/java/com/affordmed/logging_middleware/services/LoggingService.java
+++ logging_middleware/src/main/java/com/affordmed/logging_middleware/services/LoggingService.java
@@ -1,18 +1,24 @@
 package com.affordmed.logging_middleware.services;
 
+import com.affordmed.logging_middleware.client.LoggingClient;
 import com.affordmed.logging_middleware.dtos.LogRequestDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LoggingService {
 
-    public void log(LogRequestDTO request) {
+    private final AuthService authService;
+    private final LoggingClient loggingClient;
 
-        System.out.println(
-                "[" + request.getLevel() + "] "
-                        + request.getPackageName()
-                        + " : "
-                        + request.getMessage()
+    public String log(LogRequestDTO request) {
+
+        String token = authService.getAccessToken();
+
+        return loggingClient.sendLog(
+                token,
+                request
         );
     }
 }
