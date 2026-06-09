24195d5b99dcaaf7d20e081432d3dfe0fe3250c5
sahil <sahil9955555@gmail.com>
2026-06-09 12:49:12 +0530

    Added basic folder of vms

--- logging_middleware/src/main/java/com/affordmed/logging_middleware/LoggingMiddlewareApplication.java
+++ logging_middleware/src/main/java/com/affordmed/logging_middleware/LoggingMiddlewareApplication.java
@@ -1,5 +1,6 @@
 package com.affordmed.logging_middleware;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoggingMiddlewareApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries()
+                .forEach(entry ->
+                        System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(LoggingMiddlewareApplication.class, args);
     }
-
 }
